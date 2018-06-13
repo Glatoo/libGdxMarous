1b29032bcd0abf53e4f25aebb07a1826d2f32abf
Karyumi <warainosvk@gmail.com>
2018-06-13 21:38:26 +0200

    PES

--- desktop/src/com/glatoo/game/desktop/DesktopLauncher.java
+++ desktop/src/com/glatoo/game/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.glatoo.game.Main;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width=800;
+		config.height=600;
+		config.title="PES";
 		new LwjglApplication(new Main(), config);
 	}
 }

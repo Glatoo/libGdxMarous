2e1bf4ceb2435429cd769167c027b3794d912e30
Karyumi <warainosvk@gmail.com>
2018-06-15 20:26:10 +0200

    Orthographic camera

--- core/src/com/glatoo/game/Main.java
+++ core/src/com/glatoo/game/Main.java
@@ -1,24 +1,29 @@
 package com.glatoo.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 
 public class Main extends ApplicationAdapter {
+		@Override
+		public void create(){
 
+		}
 
-	
-	@Override
-	public void create () {
+		@Override
+		public void render() {
 
+		}
 
-	}
+		@Override
+		public void dispose() {
 
-	@Override
-	public void render () {
-
-	}
-	
-	@Override
-	public void dispose () {
-
-	}
+		}
 }
